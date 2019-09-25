5cbfd12907755faccd44fa4f11828f9755178593
Liz Pritchett <lizpritchett@dpengineering.org>
2019-09-25 11:35:31 -0700

    The first video tutorial

---
+++ kivytutorial1.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text = "Tech with Tim")
+
+if __name__ == "__main__":
+    MyApp().run()
