84c55f4d549c8345f2f129411abec65f4d9c38e8
Liz Pritchett <lizpritchett@dpengineering.org>
2019-09-27 11:02:40 -0700

    Update after fifth video

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class MyGrid(Widget):
-    pass
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("Name:", self.name.text, "Email:", self.email.text)
+        self.name.text = ""
+        self.email.text = ""
 
 
 class MyApp(App):
